c668a46a88a8b169995bbe62aa5e7150338d3192
Ohi20 <ashrafohi20@gmail.com>
2022-06-09 13:26:57 +0600

    Navbar-Created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Pages/Header/Header';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
     <div className='px-5'>
        <Header></Header>
+
+       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
+
     </div>
   );
 }
--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,24 +1,32 @@
 import React from 'react';
+import { Badge, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h2 className='text-danger text-center my-3'>KimberlyLaw</h2>
-            <div>
+        <div >
+            <h2 className=' text-center my-3'>KimberlyLaw</h2>
+            
+            <div className='d-flex justify-content-center'>
             <nav class="navbar navbar-expand-lg navbar-light ">
-  <a class="navbar-brand" href="#">Navbar</a>
+  
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">Features</a>
-      <a class="nav-item nav-link" href="#">Pricing</a>
-      <a class="nav-item nav-link disabled" href="#">Disabled</a>
+       <Link class="nav-item nav-link btn-primary rounded mx-2 text-white font-weight-bold" to="/">Home</Link>
+       <Link class="nav-item nav-link btn-primary rounded mx-2 text-white font-weight-bold" to="/services">Services</Link>
+       <Link class="nav-item nav-link btn-primary rounded mx-2 text-white font-weight-bold" to="/checkout">Checkout </Link>
+       <Link class="nav-item nav-link btn-primary rounded mx-2 text-white font-weight-bold" to="/blogs">Blogs</Link>  
+       <Link class="nav-item nav-link btn-primary rounded mx-2 text-white font-weight-bold" to="/login">Login</Link> 
+       <Link class="nav-item nav-link" to="*"></Link>
     </div>
   </div>
 </nav>
+           
+
+           
             </div>
         </div>
     );
