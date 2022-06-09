ceedc347d2fc79f888fa853d648cd3c009311a89
Ohi20 <ashrafohi20@gmail.com>
2022-06-09 12:53:29 +0600

    project-setu

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './Pages/Header/Header';
+
+function App() {
+  return (
+    <div className='px-5'>
+       <Header></Header>
+    </div>
+  );
+}
+
+export default App;
