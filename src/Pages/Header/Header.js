c7069f2475c842e7ea052b950f1263823955e6f4
Ohi20 <ashrafohi20@gmail.com>
2022-06-11 10:42:51 +0600

    banner-added

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Badge, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import '../Header/Header.css';
 
 const Header = () => {
     return (
         <div >
-            <h2 className=' text-center my-3'>KimberlyLaw</h2>
+            <h2 className=' text-center my-3 header-title'>KimberlyLaw</h2>
             
             <div className='d-flex justify-content-center'>
             <nav class="navbar navbar-expand-lg navbar-light ">
@@ -18,11 +18,11 @@ const Header = () => {
 
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-       <Link class="nav-item nav-link btn-primary rounded mx-2 text-white font-weight-bold" to="/">Home</Link>
-       <Link class="nav-item nav-link btn-primary rounded mx-2 text-white font-weight-bold" to="/services">Services</Link>
-       <Link class="nav-item nav-link btn-primary rounded mx-2 text-white font-weight-bold" to="/checkout">Checkout </Link>
-       <Link class="nav-item nav-link btn-primary rounded mx-2 text-white font-weight-bold" to="/blogs">Blogs</Link>  
-       <Link class="nav-item nav-link btn-primary rounded mx-2 text-white font-weight-bold" to="/login">Login</Link> 
+       <Link class="nav-item nav-link menu-ul rounded mx-2 text-white font-weight-bold" to="/">Home</Link>
+       <Link class="nav-item nav-link menu-ul rounded mx-2 text-white font-weight-bold" to="/services">Services</Link>
+       <Link class="nav-item nav-link menu-ul rounded mx-2 text-white font-weight-bold" to="/checkout">Checkout </Link>
+       <Link class="nav-item nav-link menu-ul rounded mx-2 text-white font-weight-bold" to="/blogs">Blogs</Link>  
+       <Link class="nav-item nav-link menu-ul rounded mx-2 text-white font-weight-bold" to="/login">Login</Link> 
        <Link class="nav-item nav-link" to="*"></Link>
     </div>
   </div>
--- src/Pages/Home/Homebanner.js
+++ src/Pages/Home/Homebanner.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
+import '../Home/Homebanner.css';
 
 const Homebanner = () => {
     return (
         <div>
-                     <Card className="text-center">
-  <Card.Header>John Leader</Card.Header>
+                     <Card className="text-center border-white text-white">
+  <Card.Header className='text-uppercase banner-title bg-white'>John Leader</Card.Header>
   <Card.Body>
-    <Card.Title>THE RIGHT LAWYER</Card.Title>
-    <Card.Text>
+    <Card.Title className='banner-title'>THE RIGHT LAWYER</Card.Title>
+    <Card.Text className='banner-title'>
     MAKES ALL THE DIFFERENCE
     </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
+    <Button className='text-uppercase font-bold' variant="primary">Explore</Button>
   </Card.Body>
-  <Card.Footer className="text-muted">2 days ago</Card.Footer>
-</Card>
+  </Card>
         </div>
     );
 };
