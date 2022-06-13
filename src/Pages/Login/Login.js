4de7d33e861afbe91a61e44ce74c119d7760a73b
Ohi20 <ashrafohi20@gmail.com>
2022-06-13 08:30:34 +0600

    login-page-added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -4,7 +4,7 @@ import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Loading from '../../Shared/Loading';
-import Testlogin from './Testlogin';
+import Footer from '../../Shared/Footer';
 
 const Login = () => {
 
@@ -36,85 +36,102 @@ const Login = () => {
         SignInWithEmailAndPassword(data.email,data.password);
     }
 
-
     return (
-        <div className='d-flex h-screen justify-conten-center align-items-center '>
-        <div class="card lg:card-side text-white bg-neutral shadow-xl">
+        <div className='px-5'>
+            <div class="card-body d-flex justify-content-center  my-4 ">
 
-<div class="card-body">
-<h2 class="card-title text-center text-2xl font-bold">Login</h2>
+<div>
+
+<h2 class="card-title text-center text-primary">Login</h2>
+
 <form onSubmit={handleSubmit(onSubmit)}>
-
-<div class="form-control w-full max-w-xs">
-<label class="label">
-<span class="label-text text-white">Email</span>
-
-</label>
+<div class="form-group d-flex flex-column justify-content-center align-items-center mb-2">
+<div>
+<label className='' for="exampleInputEmail1">Email</label>
+</div>
+<div>
 <input 
 type="email" 
 placeholder="Your Email" 
-class="input input-bordered w-full max-w-xs" 
+className="input text-center" 
 {...register("email", {
-  required: {
-      value: true,
-      massage: 'email required'
-  },
+required: {
+value: true,
+massage: 'email required'
+},
 pattern: {
-  value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
-  message: 'Please Give a Valid Email' // JS only: <p>error message</p> TS only support string
+value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+message: 'Please Give a Valid Email' // JS only: <p>error message</p> TS only support string
 }
 })}
 />
+</div>
+
 <label class="label">
 {errors.email?.type === 'required' &&  <span class="label-text-alt text-red-500">{errors.email.message}</span>}
 {errors.email?.type === 'pattern' &&  <span class="label-text-alt text-red-500">{errors.email.message}</span>}
 
 
 </label>
-</div>
-<div class="form-control w-full max-w-xs">
-<label class="label">
-<span class="label-text text-white">Password</span>
 
-</label>
+
+</div>
+<div class="form-group d-flex flex-column justify-content-center align-items-center mb-2">
+<div>
+<label className='' for="exampleInputPassword1">Password</label>
+</div>
+<div>
 <input 
 type="password" 
 placeholder="Password" 
-class="input input-bordered w-full max-w-xs" 
+className="input text-center" 
 {...register("password", {
-  required: {
-      value: true,
-      massage: 'password required'
-  },
-  minLength: {
-  value: 6,
-  message: 'Must be 6 characters or longer' // JS only: <p>error message</p> TS only support string
+required: {
+value: true,
+massage: 'password required'
+},
+minLength: {
+value: 6,
+message: 'Must be 6 characters or longer' // JS only: <p>error message</p> TS only support string
 }
 })}
 />
+</div>
+
+</div>
+
+
+
 <label class="label">
 {errors.password?.type === 'required' &&  <span class="label-text-alt text-red-500">{errors.password.message}</span>}
 {errors.password?.type === 'minLength' &&  <span class="label-text-alt text-red-500">{errors.password.message}</span>}
 
 
 </label>
-</div>
 
- 
-  {signInError}
-  <input className='btn w-full max-w-xs text-white' type="submit" value="Login" />
+{signInError}
+
+
+
+<div className='d-flex justify-content-center'>
+<button type="submit" className="btn btn-primary text-center mb-1">Submit</button>
+</div>
 </form>
-<p><small>New to Camerassories? <Link className='text-secondary' to="/signup">Create New Account</Link></small></p>
+<p><small>New to KimberLaw? <Link className='text-secondary' to="/signup">Create New Account</Link></small></p>
 <div class="dropdown-divider">OR</div>
+<div className='d-flex justify-content-center align-items-center'>
 <button onClick={() => signInWithGoogle()} 
 className='btn-primary'>Continue with Google</button>
 </div>
 </div>
 
-<div>
-    <Testlogin></Testlogin>
+
+
 </div>
-    </div>
+<Footer></Footer>
+        </div>
+
+
     );
 };
 
--- src/Shared/Loading.js
+++ src/Shared/Loading.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Loading = () => {
     return (
-        <div className="spinner-grow" role="status">
+       <div className='text-center'>
+         <div className="spinner-grow " role="status">
         <span className="visually-hidden">Loading...</span>
       </div>
+       </div>
     );
 };
 
